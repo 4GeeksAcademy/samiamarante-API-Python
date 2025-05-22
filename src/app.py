45b1ffbdbc70163f1df84ae35b16a50c6cde6d4d
samiamarante <135390658+samiamarante@users.noreply.github.com>
2025-05-22 16:14:51 +0000

    Completed exercises

--- src/app.py
+++ src/app.py
@@ -2,9 +2,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+import seaborn as sns
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
 
-
 # Spotify API credentials
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
